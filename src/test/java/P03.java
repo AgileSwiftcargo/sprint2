66582c6d94d5a3cb64db2e3d213316a1518eb2f5
Cihat YİGİT <agileswiftcargo@gmail.com>
2025-03-17 16:35:07 +0300

    cihat1

--- src/test/java/P03.java
+++ src/test/java/P03.java
@@ -3,5 +3,9 @@ import org.w3c.dom.ls.LSOutput;
 public class P03 {
     public static void main(String[] args) {
         System.out.println("deneme Alpay");
+        System.out.println("deneme cihat");
+
     }
+
+
 }
