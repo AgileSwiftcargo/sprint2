afaf476d9b4e27779b519fb0b701bd591ee69868
Furkan Detseli <68570706+furkandetseli@users.noreply.github.com>
2025-03-17 18:31:34 +0300

    furkandetseli update
    yeni class olusturuldu, branch acildi ve projeye giris yapildi.

---
+++ src/test/java/P04.java
@@ -0,0 +1,10 @@
+
+public class P04 {
+    public static void main(String[] args) {
+        System.out.println("deneme Furkan");
+        System.out.println("deneme5 FurkanDetseli1231231232132132133123213213");
+
+    }
+
+
+}
