bf81a8e9983385862190c3f94a2c3b7d7d2cb659
Furkan Detseli <68570706+furkandetseli@users.noreply.github.com>
2025-03-19 15:29:54 +0300

    furkanv.1

--- src/test/java/pages/Furkan.java
+++ src/test/java/pages/Furkan.java
@@ -1,4 +1,23 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class Furkan {
+    public static void main(String[] args) {
+        System.out.println("Başlangıç...");
+    }
+    public Furkan(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    //Why Agile Swift Cargo Bölümü
+    @FindBy(xpath = "/html/body/section[4]/div")
+    public WebElement WhyAgileSwiftCargoBolum;
+
+    //Why Agile Swift Cargo Bölümü Bilgilendirici Kutular
+    @FindBy(xpath = "/html/body/section[4]/div/div[2]")
+    public WebElement WhyAgileSwiftCargoBox;
 }
---
+++ src/test/java/tests/US006/TC6_1.java
@@ -0,0 +1,4 @@
+package tests.US006;
+
+public class TC6_1 {
+}
