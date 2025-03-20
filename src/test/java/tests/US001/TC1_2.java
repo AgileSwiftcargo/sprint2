b3be8c1ed78325d9366788661828dfa617c3d9e9
Ömer Can Yağcı <omercan.yagcii@gmail.com>
2025-03-20 14:55:23 +0300

    versiyon1.2

--- src/test/java/tests/US001/TC1_2.java
+++ src/test/java/tests/US001/TC1_2.java
@@ -1,4 +1,25 @@
 package tests.US001;
 
-public class TC1_2 {
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.Driver;
+import utilities.TestBaseRapor;
+
+import java.time.Duration;
+import java.time.LocalTime;
+
+public class TC1_2 extends TestBaseRapor {
+    @Test
+    public void anasayfaGoruntulenmeSureTesti (){
+
+        LocalTime start = LocalTime.now();
+
+        Driver.getDriver().get("https://qa.agileswiftcargo.com/");
+
+        LocalTime finish = LocalTime.now();
+        int millis = (int) Duration.between(start, finish).toSeconds();
+        System.out.println("Anasayfa yuklenme suresi " + millis + " seconds");
+
+        Assert.assertTrue(millis < 4);
+    }
 }
