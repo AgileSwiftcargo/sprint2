b1ba4f8672e3beed17c0d56e116a84a2b5440f24
Furkan Detseli <68570706+furkandetseli@users.noreply.github.com>
2025-03-21 21:20:28 +0300

    furkanv.3

--- src/test/java/tests/US006/TC6_1.java
+++ src/test/java/tests/US006/TC6_1.java
@@ -1,4 +1,39 @@
 package tests.US006;
 
-public class TC6_1 {
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.Furkan;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.TestBaseRapor;
+
+public class TC6_1 extends TestBaseRapor {
+    Furkan furkan ;
+
+    @Test
+    public void servisYazisiKontrolTesti (){
+
+        extentTest = extentReports.createTest("Neden Agile Swift Cargo Yazısı Kontrol Testi" +
+                "Kullanici ana sayfada neden agile swift cargo alanını görüntülemeli");
+
+        // Kullanıcı, https://qa.agileswiftcargo.com/ adresine gider.
+        Driver.getDriver().get(ConfigReader.getProperty("Url"));
+        extentTest.info("Kullanici agileswiftcargo ana sayfasina gider");
+
+        // Ana sayfada "Why Agile Swift Cargo" yazısının bulunduğunu kontrol eder
+        furkan = new Furkan();
+        String expectedYazi = "Why Agile Swift Cargo";
+        String actualYazi = furkan.WhyAgileSwiftCargoText.getText();
+
+        Assert.assertEquals(actualYazi,expectedYazi);
+        extentTest.pass("Servis alanının doğru bir şekilde görüntülendiğini doğrular");
+
+
+        // sayfayi kapatir
+        extentTest.info("sayfayi kapatir");
+
+
+
+
+    }
 }
---
+++ src/test/java/tests/US006/TC6_2.java
@@ -0,0 +1,48 @@
+package tests.US006;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.Furkan;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.TestBaseRapor;
+import java.util.List;
+
+public class TC6_2 extends TestBaseRapor {
+    Furkan furkan;
+
+    @Test
+    public void testLinksInCourierBoxes() {
+
+        extentTest = extentReports.createTest("Why Agile Swift Cargo Boxlarının Link Kontrolü",
+                "Kullanıcılar, bu alanda sunulan bilgilerle ilgili daha fazla detay almak için uygun bağlantılara yönlendirilmelidir.");
+
+        // Kullanıcı, https://qa.agileswiftcargo.com/ adresine gider.
+        Driver.getDriver().get(ConfigReader.getProperty("Url"));
+        extentTest.info("Kullanici Agile Swift Cargo ana sayfasina gider");
+
+        furkan = new Furkan();
+
+        // Tüm Why Courier Boxlarını seçer
+        List<WebElement> courierBoxes = Driver.getDriver().findElements(By.cssSelector(".why-courier-col"));
+
+        for (WebElement box : courierBoxes) {
+            // Her bir kutunun içinde <a> etiketi olup olmadığını kontrol eder
+            List<WebElement> links = box.findElements(By.tagName("a"));
+
+            if (links.isEmpty()) {
+                extentTest.fail("Kutuda link bulunamadı: " + box.getText());
+            } else {
+                extentTest.pass("Kutuda en az bir link bulundu: " + box.getText());
+            }
+
+            // Her kutuda en az bir link olması gerektiğini doğrular
+            Assert.assertFalse(links.isEmpty(), "Link bulunamadı: " + box.getText());
+        }
+
+        // Test bittiğinde sayfayı kapatır
+        extentTest.info("Sayfa kapatıldı");
+    }
+}
