6673f3318038cef15599f5ad4c71ea6be7421669
WBusra <daginbusraa34@gmail.com>
2025-03-28 12:51:53 +0300

    busrav11

--- src/test/java/tests/US015/TC15_6.java
+++ src/test/java/tests/US015/TC15_6.java
@@ -1,12 +1,60 @@
 package tests.US015;
 
 import org.openqa.selenium.JavascriptExecutor;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
+import pages.Busra;
 import pages.Locator;
+import utilities.ConfigReader;
 import utilities.CrossTestBaseRapor;
+import utilities.Driver;
 import utilities.ReusableMethods;
 
 public class TC15_6 extends CrossTestBaseRapor {
 
+ Locator locator;
+
+    @Test
+    public void ilgiliBaslıgatıklama() {
+        Locator locator = new Locator();
+        SoftAssert softAssert = new SoftAssert();
+
+        extentTest = extentReports.createTest("İlgili başlığa tıklama testi",
+                "Başlıklara tıklandığında doğru sayfa açılmalı");
+
+        // kullanıcı anasayfaya gider
+        driver.get(ConfigReader.getProperty("Url"));
+        extentTest.info("Kullanici agileswiftcargo ana sayfasina gider");
+
+        //"E-Commerce delivery","Pick & Drop","Packageing",
+        //"Warehousing" başlıklarına tıklar
+
+        locator.ECommerceDeliveryButonu.click();
+
+        softAssert.assertEquals(driver.getCurrentUrl(), "https://qa.agileswiftcargo.com/service-details/1");
+        extentTest.pass("E-Commerce delivery sayfası açılır");
+
+        locator.PickDropButonu.click();
+        softAssert.assertEquals(driver.getCurrentUrl(), "https://qa.agileswiftcargo.com/service-details/2");
+        extentTest.pass("Pick & Drop sayfası açılır");
+
+        locator.PackageinButonu.click();
+
+        softAssert.assertEquals(driver.getCurrentUrl(), "https://qa.agileswiftcargo.com/service-details/3");
+        extentTest.pass("Packageing sayfası açılır");
+
+        locator.WarehousinButonu.click();
+        softAssert.assertEquals(driver.getCurrentUrl(), "https://qa.agileswiftcargo.com/service-details/4");
+        extentTest.pass("Warehousing sayfası açılır");
+
+
+        extentTest.info("Sayfayı kapatır");
+
+
+    }
 }
+
+
+
+
--- src/test/java/tests/US026/TC26_2.java
+++ src/test/java/tests/US026/TC26_2.java
@@ -1,14 +1,41 @@
 package tests.US026;
 
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import pages.Locator;
+import utilities.ConfigReader;
 import utilities.CrossTestBaseRapor;
 
 public class TC26_2 extends CrossTestBaseRapor {
-
+Locator locator = new Locator();
 @Test
     public void detaylıodemebilgisigoruntuleme(){
+    SoftAssert softAssert = new SoftAssert();
+
+
     extentTest = extentReports.createTest("detaylı ödeme görüntüleyebilme testi," ,
             "kullanıcı detaylı ödeme bilgilerine  ulaşabilmeli ");
+
+    locator = new Locator();
+
+    locator.loginButonu.click();
+    extentTest.info("login olundu");
+
+    locator.emailTextbox.sendKeys(ConfigReader.getProperty("MerchantBusraGecerliMail"));
+    extentTest.info("Geçerli bir email girer");
+
+    locator.passwordTextbox.sendKeys(ConfigReader.getProperty("MerchantBusraGecerliPassword"));
+    extentTest.info("Geçerli bir password girer");
+
+    locator.signInButton.click();
+    extentTest.info("Sing in butonuna tiklar");
+
+softAssert.assertEquals(driver.getCurrentUrl(),"https://qa.agileswiftcargo.com/merchant/online-payment/stripe");
+extentTest.pass("stripe logosunu görür");
+
+
+
+
 
 
 
